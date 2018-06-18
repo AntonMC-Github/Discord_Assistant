2fb8f0328593b6f0c087e176766dfde9edd6f764
DieserMarius <40335235+DieserMarius@users.noreply.github.com>
2018-06-18 17:39:49 +0200

    2.1 Admincmd überatbeitet

--- admincmd/admin_game.py
+++ admincmd/admin_game.py
@@ -1,3 +1,9 @@
+import discord
+
+
+
+
+
     if message.content.startswith('!game') and message.author.id == ImXemm:
         game = message.content[6:]
         await client.change_presence(game=discord.Game(name=game), status='online')
