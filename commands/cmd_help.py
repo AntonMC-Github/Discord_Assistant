de68d4cd8d37a642e43ac7a680189ec1339372b9
DieserMarius <40335235+DieserMarius@users.noreply.github.com>
2018-06-23 15:21:31 +0200

    2.7 Help überarbeitet

--- commands/cmd_help.py
+++ commands/cmd_help.py
@@ -5,4 +5,19 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*UNTERDRÜCKTE ARGUMENTE: %s*" % args.__str__()[1:-1].replace("'", "")
-    yield from client.send_message(message.channel, "Also ich helfe dir nicht :D" + args_out)
+    embed = discord.Embed(
+        title="",
+        color=0xe67e22,
+        description="Hier kannst du dich bewerben:\n"
+                    "https://goo.gl/forms/G8ugLQL7x4OsjU1r1"
+    )
+    embed.set_author(
+        name="Assistant",
+        icon_url="",
+        url="http://tjcmagazin.de/"
+    )
+    embed.set_footer(
+        text="© Assistant - 2018",
+        icon_url=""
+    )
+    yield from client.send_message(message.channel, embed=embed)
