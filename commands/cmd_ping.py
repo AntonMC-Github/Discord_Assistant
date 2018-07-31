328967a4cd3912b6c5646a0395e9eba542896a11
DieserMarius <account@meier-ilvesheim.de>
2018-07-31 16:28:06 +0200

    2.9 Ping überarbeitet

--- commands/cmd_ping.py
+++ commands/cmd_ping.py
@@ -5,4 +5,18 @@ def ex(args, message, client, invoke):
     args_out = ""
     if len(args) > 0:
         args_out = "\n\n*UNTERDRÜCKTE ARGUMENTE: %s*" % args.__str__()[1:-1].replace("'", "")
-    yield from client.send_message(message.channel, "Pong!" + args_out)
+    embed = discord.Embed(
+        title="",
+        color=0xe67e22,
+        description="Pong!"
+    )
+    embed.set_author(
+        name="Assistant",
+        icon_url="",
+        url="http://antonmc.de/"
+    )
+    embed.set_footer(
+        text="© Assistant Bot - 2018",
+        icon_url=""
+    )
+    yield from client.send_message(message.channel, embed=embed)
