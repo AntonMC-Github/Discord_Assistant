fb03f08a719b107b1e3b5b7168876ca93e7505a1
DieserMarius <account@meier-ilvesheim.de>
2018-08-04 23:34:21 +0200

    3.1 Newfile geändert

--- newfile.py
+++ newfile.py
@@ -1,4 +1,14 @@
-from pathlib import Path
-my_file = Path("/commands")
+if my_file.is_dir():
+    my_file = Path("/path/to/file")
+    invoke = message.content[len(STATICS.PREFIX):].split(" ")[0]
+    args = message.content.split(" ")[1:]
 
-if my_file.is_file():
+    if my_file.is_file():
+        yield from commands.get(invoke).ex(args, message, client, invoke)
+
+    else:
+        yield from client.send_message(message.channel, embed=Embed(color=discord.Color.red(),
+                                                                        description=("Tut mir leid, diesen Befehl gibt es nicht!"))) 
+
+else:
+    print:("Error 301 | directory does not exist!")
