b087ec358c7359efcd414043710aa52a34f08a26
DieserMarius <account@meier-ilvesheim.de>
2018-08-23 08:46:15 +0200

    3.3 Schwachsinn rausgeaddet

--- newfile.py
+++ newfile.py
@@ -10,14 +10,6 @@ if my_dir.is_dir():
     else:
         yield from client.send_message(message.channel, embed=Embed(color=discord.Color.red(),
                                                         description=("Error 304 | Command cant found.")))
-"""
-    elif:
-        yield form command_send.message.get.(invoke).ex(args, message, client, invoke)
-        import newfile as newfile.py
-        my_dir = Path("/path/to/dir")
-        args_out COMMIT_EDITMSG
-        packed-refs as remotes_heads
-        fetch_head tags objects remotes_heads
-"""
+
 else:
     print:("Error 301 | directory does not exist!")
